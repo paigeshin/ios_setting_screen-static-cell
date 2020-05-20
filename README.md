8b034abe89f7842a345dabc8702dfd777de8262f
shinseunghyun <paigeshin1991gmail.com>
2020-05-20 22:35:46 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ios_setting_screen-static-cell
---
+++ Swift_Setting_Screen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift_Setting_Screen
+//
+//  Created by shin seunghyun on 2020/05/20.
+//  Copyright © 2020 shin seunghyun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
